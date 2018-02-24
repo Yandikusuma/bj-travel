ba6609896e4c7bbc1357dede1ad31baef34d2eea
Eri Wandri <wawandry@gmail.com>
2018-02-24 12:32:37 +0700

    adding strukture fil

---
+++ src/components/Header/Menu/Menu.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Menu extends Component {
+    render() {
+        return (
+            <div>
+                <p>This menu</p>
+            </div>
+        )
+    }
+}
+
+export default Menu
---
+++ src/components/Header/Slider/Slider.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Slider extends Component {
+    render() {
+        return (
+            <div>
+                <p>This slider</p>                
+            </div>
+        )
+    }
+}
+
+export default Slider
