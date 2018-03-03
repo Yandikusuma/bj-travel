498d2dd3be586bc436e74bbdd18b4153496dd58f
Yandikusuma <yandibaharselatan@gmail.com>
2018-03-03 12:43:01 +0700

    adding cara pesan

--- src/components/Details/Detail.js
+++ src/components/Details/Detail.js
@@ -3,7 +3,7 @@ import { Header} from 'semantic-ui-react';
 
 import TiketDetail from './Detail_Head/Head_Detail';
 import TentangTiket from './Detail_Tiket/Details_Tiket';
-import TentangUs from './Detail_Main/Tentang_tiket';
+import TentangUs from './Detail_Main/Cara_pemesanan';
 
 class DetailTiket extends Component {
 	render(){
